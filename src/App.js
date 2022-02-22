6fb9ebf980fe4d18c66ce06a52fbefe5bdf6b034
cenkberry <cenkberi@mail.com>
2022-02-22 11:08:07 +0300

    create-oto-divs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-
 export function App() {
   return (
-     <div class="bg-dark text-light">helloss</div>
+    
+     <div className="container bg-dark text-light text-center">
+     <button className="btn btn-warning my-2 mybut">button</button>
+     <div className="para"></div>
+     </div>
      
   );
 }
+
+let mypara = document.querySelector('.para');
+let items = ["pizza","tomato","bread","spagetti","fish"];
+for(let x in items){
+  let newDiv = document.createElement('div');
+  newDiv.innerHTML = items[x];
+  mypara.append(newDiv);
+};
