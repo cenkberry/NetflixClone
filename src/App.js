02da147986882d9a9c119df1780e149cca62bc3e
cenkberry <cenkberi@mail.com>
2022-02-22 11:35:52 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 export function App() {
   return (
-    
      <div className="container bg-dark text-light text-center">
      <button className="btn btn-warning my-2 mybut">button</button>
-     <div className="para"></div>
      </div>
-     
   );
 }
-
-let mypara = document.querySelector('.para');
-let items = ["pizza","tomato","bread","spagetti","fish"];
-for(let x in items){
-  let newDiv = document.createElement('div');
-  newDiv.innerHTML = items[x];
-  mypara.append(newDiv);
-};
