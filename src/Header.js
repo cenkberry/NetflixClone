5e6c7002066c32bd1c07b3d4c69c7cc06076b5dc
cenkberry <cenkberi@mail.com>
2022-02-23 17:24:13 +0300

    ok

--- src/Header.js
+++ src/Header.js
@@ -1,45 +1,11 @@
-const menuitems = 
-[
-  {
-    id: "Ana Sayfa",
-    url: "./index.html"
-  },
-  {
-   id: "Bitkiler",
-   link: "./bitkiler.html"
- },
- {
-  id: "Arajmanlar",
-  link: "./arajmanlar.html"
-},
-{
-  id: "Buketler",
-  link: "./buketler.html"
-},
-{
-  id: "Nişan&Evlilik",
-  link: "./nisanev.html"
-},
-{
-  id: "İletişim",
-  link: "./contact.html"
-}
-];
-  const menu = document.querySelector('.menu');
-  let result = menuitems.map( item => `<a class="nav-link" aria-current="page" href="${item.link}">${item.id}</a>`).join("");
-  menu.innerHTML = result;
-
 export function Header(){  
   return(
-  <header className="container-xl">
-    <div className="row">
-      
-      <div className="col-12 flexy mt-1 border">
-          <img className="logobox" src="./logo.png" alt="" />
+    <>
+    <header className="container-xl vh-100">
+        <div className="row bg-warning">
+          <ul className="col-12 menu flexy bg-dark flex-wrap"> x </ul>
       </div>
-
-      <ul className="col-12 flexy mt-1 border menu"></ul>
-   </div>
-  </header>
+    </header>
+  </>
 )
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import {Header} from './Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header/>
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+const menuitems = [
+  {
+    id: "Ana Sayfa",
+    link: "./index.html"
+  },
+  {
+   id: "Bitkiler",
+   link: "./bitkiler.html"
+ },
+ {
+  id: "Arajmanlar",
+  link: "./arajmanlar.html"
+},
+{
+  id: "Buketler",
+  link: "./buketler.html"
+},
+{
+  id: "Nişan&Evlilik",
+  link: "./nisanev.html"
+},
+{
+  id: "İletişim",
+  link: "./contact.html"
+}
+];
+
+const menu = document.querySelector('.menu');
+let result = menuitems.map( item => `<a class="nav-link my-1 mx-1" href="${item.link}">${item.id}</a>`).join("");
+menu.innerHTML =  result;
