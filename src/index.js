40b4c7a1a9803893d22143ff86d445f85cc05e1a
cenkberry <cenkberi@gmail.com>
2022-02-23 17:44:25 +0300

    config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Header} from './Header';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,34 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-const menuitems = [
-  {
-    id: "Ana Sayfa",
-    link: "./index.html"
-  },
-  {
-   id: "Bitkiler",
-   link: "./bitkiler.html"
- },
- {
-  id: "Arajmanlar",
-  link: "./arajmanlar.html"
-},
-{
-  id: "Buketler",
-  link: "./buketler.html"
-},
-{
-  id: "Nişan&Evlilik",
-  link: "./nisanev.html"
-},
-{
-  id: "İletişim",
-  link: "./contact.html"
-}
-];
-
-const menu = document.querySelector('.menu');
-let result = menuitems.map( item => `<a class="nav-link my-1 mx-1" href="${item.link}">${item.id}</a>`).join("");
-menu.innerHTML =  result;
