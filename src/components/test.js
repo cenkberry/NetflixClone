461e91dc1e4012fd862ab91c43949afa9aa59e4e
cenkberry <cenkberi@gmail.com>
2022-02-24 20:52:38 +0300

    done

--- src/components/test.js
+++ src/components/test.js
@@ -1,11 +1,1 @@
-const MyLogo = () => <img src="./img/1.png" alt="zo" />
-const title = "Hello World";
-
-export const Codes = () => {
-  return(
-  <>
-        <MyLogo />
-        <h1 className="text-center">{title}</h1>
-  </>
-  )
-}
+export const Codes = () => <img src="./img/logo.png" alt="BigCo Inc. logo"/>;
