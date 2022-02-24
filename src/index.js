5903227d1e978281bbc348eb3ebfe68aa6264a8b
cenkberry <cenkberi@gmail.com>
2022-02-24 19:23:02 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
- function Greeting(){
-   return <h1>Hello World :D</h1>
- }
-  
 
   ReactDOM.render
   (
     <React.StrictMode>
-    <Greeting />
+    
     </React.StrictMode>,
       document.getElementById('root')
   )
