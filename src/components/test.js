c295f6e06e0dcd270f18f296ef46c16025c97251
cenkberry <cenkberi@gmail.com>
2022-02-24 20:37:41 +0300

    changed test.js

---
+++ src/components/test.js
@@ -0,0 +1,11 @@
+const MyLogo = () => <img src="./img/1.png" alt="zo" />
+const title = "Hello World";
+
+export const Codes = () => {
+  return(
+  <>
+        <MyLogo />
+        <h1 className="text-center">{title}</h1>
+  </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import {Codes} from './components/test';
 
 ReactDOM.render(
   <React.StrictMode>
