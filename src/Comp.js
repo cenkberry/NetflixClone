73a04f776eb75f0ec4fe33808f69ceec81a845c9
cenkberry <cenkberi@gmail.com>
2022-02-25 18:16:22 +0300

    ok

--- src/Comp.js
+++ src/Comp.js
@@ -1,23 +1,5 @@
 export function Header(){
   return(
-    <> 
-        <div className="row">
-            <div className="col-10 col-md-3 bg-warning flex">
-
-              
-            </div>
-
-            <div className="d-none col-md-9 d-md-flex bg-danger menu">
-              <a href="#">Ana Sayfa</a>
-              <a href="#">Buketler</a>
-              <a href="#">Bitkiler</a>
-              <a href="#">Arajmanlar</a>
-              <a href="#">Evlilik Org.</a>
-              <a href="#">İletişim</a>
-            </div>
-
-            <div className="flex d-md-none col-2 bg-primary">2222</div>
-        </div>
-    </>
+    <h1>s</h1> 
   )
 }
