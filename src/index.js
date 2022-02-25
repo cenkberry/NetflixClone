364aa6dbfde1df524191a0beeaa8de687586bd83
cenkberry <cenkberi@gmail.com>
2022-02-25 14:38:18 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -2,24 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Mydiv = (props) => {
-  return( 
-    <div>
-       <h3>{props.title}</h3>
-       <img src={props.tlink} alt='pic' />
-       <h4>{props.parag}</h4>
-    </div>
-  )
-};
-
 ReactDOM.render(
   <React.StrictMode>
-  
-   <Mydiv
-   title= 'Daisy Photo'
-   parag= 'This is daisy'
-   tlink= 'https://pbs.twimg.com/profile_images/883859744498176000/pjEHfbdn_400x400.jpg'
-   />
 
   </React.StrictMode>,
   document.getElementById('root')
