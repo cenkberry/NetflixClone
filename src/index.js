c5fe57bab37198fc2fb79b0e798fbf53f63fb652
cenkberry <cenkberi@gmail.com>
2022-02-26 15:01:06 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Page1,Page2,Page3} from './Comp';
 const MyPage = document.querySelector('#root');
 
 ReactDOM.render(
   <React.StrictMode>
-   <Page1 />
-   <Page2 />
-   <Page3 />
+
   </React.StrictMode>, MyPage);
-
-const btn     = document.querySelector('.btn');
-const left    = document.querySelector('.left');
-const right   = document.querySelector('.right');
-
-btn.addEventListener("click", () =>{
-
-  left.style.animation  = "moveleft 1s linear";
-  right.style.animation = "moveright 1s linear";
-  
-    setTimeout(function(){
-    left.style = "none";
-    right.style = "none"; 
-    }, 2000);
-
-});
