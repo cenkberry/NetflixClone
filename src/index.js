a37801404e2f9f67a8962de2144cdc464579922f
cenkberry <cenkberi@gmail.com>
2022-02-26 12:42:03 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Cardrow} from './Comp';
 const MyPage = document.getElementById('root');
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cardrow />
 
   </React.StrictMode>, MyPage);
