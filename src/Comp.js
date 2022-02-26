16154df42e6add587891bf18dc0e2d1f78a2e231
cenkberry <cenkberi@gmail.com>
2022-02-26 13:45:47 +0300

    ok

---
+++ src/Comp.js
@@ -0,0 +1,59 @@
+export const Page1 = () => {
+   return (
+    <>
+    <div className="container-xl vh-100 bg-warning page1">
+    <a href="#page2"><button className="btn btn-info">Page2</button></a>
+    <div className="h4">Lorem, ipsum dolor.</div>
+    <div className="h4">Illo, quibusdam expedita.</div>
+    <div className="h4">Ratione, voluptates provident.</div>
+    <div className="h4">Beatae, nesciunt magnam!</div>
+    <div className="h4">Quae, quos inventore!</div>
+    <div className="h4">Aperiam, pariatur natus.</div>
+    <div className="h4">Nobis, beatae dicta?</div>
+    <div className="h4">Explicabo, maxime repudiandae?</div>
+    <div className="h4">Eius, accusamus hic.</div>
+    <div className="h4">Quasi, laborum eum.</div>
+    <div className="h4">Distinctio, quos amet!</div>
+    <div className="h4">Nam, cupiditate ab.</div>
+    <div className="h4">Repellendus, eos debitis?</div>
+    <div className="h4">Modi, vitae architecto.</div>
+    <div className="h4">Aliquid, eaque hic.</div>
+
+    </div>
+    </>
+   )
+}
+export const Page2 = () => {
+  return (
+    <>
+    <div className="container-xl vh-100 bg-info flex" id="page2">
+    <div className="h1 move">MOVE BOX</div>
+    </div>
+    </>
+  )
+}
+export const Page3 = () => {
+  return (
+     <div className="container-xl vh-100 bg-secondary page3">
+
+       <div className="h4">Lorem, ipsum dolor.</div>
+       <div className="h4">Illo, quibusdam expedita.</div>
+       <div className="h4">Ratione, voluptates provident.</div>
+       <div className="h4">Beatae, nesciunt magnam!</div>
+       <div className="h4">Quae, quos inventore!</div>
+       <div className="h4">Aperiam, pariatur natus.</div>
+       <div className="h4">Nobis, beatae dicta?</div>
+       <div className="h4">Explicabo, maxime repudiandae?</div>
+       <div className="h4">Eius, accusamus hic.</div>
+       <div className="h4">Quasi, laborum eum.</div>
+       <div className="h4">Distinctio, quos amet!</div>
+       <div className="h4">Nam, cupiditate ab.</div>
+       <div className="h4">Repellendus, eos debitis?</div>
+       <div className="h4">Modi, vitae architecto.</div>
+       <div className="h4">Aliquid, eaque hic.</div>
+       <div className="h4">Voluptas, provident autem!</div>
+       <div className="h4">Itaque, maiores assumenda!</div>
+      
+      </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-const MyPage = document.getElementById('root');
-
-
+import {Page1,Page2,Page3} from './Comp';
+const MyPage = document.querySelector('#root');
 
 ReactDOM.render(
   <React.StrictMode>
-
-
-
+   <Page1 />
+   <Page2 />
+   <Page3 />
   </React.StrictMode>, MyPage);
+
+  
+const btn    = document.querySelector('.btn');
+const move   = document.querySelector('.move');
+
+btn.addEventListener("click", () =>{
+  
+  move.style.animation = "move 1s linear";
+
+})
